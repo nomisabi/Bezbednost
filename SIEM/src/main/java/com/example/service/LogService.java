bb68eaf67e30cbe486d4c75820a3a3c0d70f2359
Nevena Đerić <nevena5695@gmail.com>
2018-04-02 21:15:41 +0200

    parse logs

--- SIEM/src/main/java/com/example/service/LogService.java
+++ SIEM/src/main/java/com/example/service/LogService.java
@@ -13,26 +13,37 @@ public class LogService {
 
 	public void save(String logStr) {
 		System.out.println(logStr);
-		String[] splited = logStr.split("\\s+");
+		String lines[] = logStr.split("\\r?\\n");
+		for (String line : lines) {
+			Log log = parseLine(line);
+			logRepository.save(log);
+		}
+	}
+
+	private Log parseLine(String line) {
+		String[] splited = line.split("\\s+");
 		String[] splited2 = splited[0].split(">");
 		int prival = Integer.parseInt(splited2[0].substring(1, splited2[0].length()));
 		int version = Integer.parseInt(splited2[1]);
+		String sd = splited[6];
 		String msg = splited[7];
-		if (splited.length > 8) {
-			StringBuilder bld = new StringBuilder();
-			int i = 7;
-			while (i < splited.length) {
-				bld.append(" ");
-				bld.append(splited[i]);
-				i++;
+
+		if (line.contains("[")) {
+			sd = line.substring(line.indexOf("["), line.indexOf("]") + 1);
+			msg = line.substring(line.indexOf("]") + 1, line.length());
+		} else {
+			if (splited.length > 8) {
+				StringBuilder bld = new StringBuilder();
+				int i = 7;
+				while (i < splited.length) {
+					bld.append(" ");
+					bld.append(splited[i]);
+					i++;
+				}
+				msg = bld.toString();
 			}
-			msg = bld.toString();
 		}
-
-		Log log = new Log(prival, version, splited[1], splited[2], splited[3], splited[4], splited[5], splited[6],
-				msg.trim());
-		logRepository.save(log);
-
+		return new Log(prival, version, splited[1], splited[2], splited[3], splited[4], splited[5], sd, msg.trim());
 	}
 
 }
