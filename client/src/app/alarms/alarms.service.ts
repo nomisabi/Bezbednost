a9757d0d9d2bb10b37366865a5d4199d3f69e3f0
nomisabi <nomisabi@192.168.0.100>
2018-06-05 06:10:46 +0200

    plus

---
+++ client/src/app/alarms/alarms.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { AlarmInterface } from '../models/alarm';
+
+@Injectable()
+export class AlarmsService {
+
+  private url = '/api/alarms';
+
+  headers: HttpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+  
+  private RegenerateData = new Subject<void>();
+
+  RegenerateData$ = this.RegenerateData.asObservable();
+
+  constructor(private http: HttpClient) { 
+  }
+
+  announceChange(){
+    this.RegenerateData.next();
+  }
+
+  getAlarms():Promise<AlarmInterface[]> {
+    return this.http
+    .get<AlarmInterface[]>(this.url, )
+    .toPromise()
+    .then(res => res)
+    .catch(this.handleError);
+  }
+
+  getAlarm(id: number):Promise<AlarmInterface> {
+    let url=this.url+'/'+id;
+    return this.http
+    .get<AlarmInterface>(url)
+    .toPromise()
+    .then(res => res)
+    .catch(this.handleError);
+  }
+
+  deleteAlarm(id: number):Promise<AlarmInterface> {
+    let url=this.url+'/'+id;
+    return this.http
+    .delete<AlarmInterface>(url)
+    .toPromise()
+    .then(res => res)
+    .catch(this.handleError);
+  }
+
+  createAlarm(alarm: AlarmInterface):Promise<void> {
+    let url=this.url;
+    return this.http
+    .post<AlarmInterface>(url, alarm, {'headers':this.headers})
+    .toPromise()
+    .then(res => res)
+    .catch(this.handleError);
+  }
+
+
+  private handleError(error: any): Promise<any> {
+    console.error("Error... ", error);
+    return Promise.reject(error.message || error);
+  }
+
+}
